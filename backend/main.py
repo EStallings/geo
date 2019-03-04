1530ff2a64c14a848817d19a5262f221ffb7b9ba
estallin@cisco.com <estallin@cisco.com>
2019-03-03 23:19:04 -0500

    updates to backend, removed db from git project because having it in was stupid

--- backend/main.py
+++ backend/main.py
@@ -24,12 +24,73 @@ def add_ip():
 			status=500
 		)
 
-@app.route("/ip", methods = ['GET'])
-def get_ip_data():
+@app.route("/ip/naive", methods = ['GET'])
+def get_ip_data_naive():
 	try:
 		page_size = int(request.args.get('count', PAGE_SIZE))
 		page = int(request.args.get('page', 1))
 		ip_data = list(db.ip_data.find().skip( (page-1) * page_size).limit(page_size))
+		return app.response_class(
+			response=dumps(ip_data),
+			status=200,
+			mimetype='application/json'
+		)
+	except Exception, e:
+		return app.response_class(
+			response=str(e),
+			status=500
+		)
+
+@app.route("/ip/spatial", methods = ['GET'])
+def get_ip_data_spatial():
+	try:
+		min_lon = (request.args.get('min_lon'))
+		max_lon = (request.args.get('max_lon'))
+		min_lat = (request.args.get('min_lat'))
+		max_lat = (request.args.get('max_lat'))
+
+		query = {
+			'$and': {
+				'longitude': { '$gte': min_lon },
+				'longitude': { '$lte': max_lon },
+				'latitude':  { '$gte': min_lat },
+				'latitude':  { '$lte': max_lat }
+			}
+		}
+
+		page_size = int(request.args.get('count', PAGE_SIZE))
+		page = int(request.args.get('page', 1))
+		ip_data = list(db.ip_data.find(query).skip( (page-1) * page_size).limit(page_size))
+		return app.response_class(
+			response=dumps(ip_data),
+			status=200,
+			mimetype='application/json'
+		)
+	except Exception, e:
+		return app.response_class(
+			response=str(e),
+			status=500
+		)
+
+@app.route("/ip/spatial/count", methods = ['GET'])
+def get_ip_count_spatial():
+	try:
+		min_lon = (request.args.get('min_lon'))
+		max_lon = (request.args.get('max_lon'))
+		min_lat = (request.args.get('min_lat'))
+		max_lat = (request.args.get('max_lat'))
+
+		query = {
+			'longitude': { '$gte': min_lon },
+			'longitude': { '$lte': max_lon },
+			'latitude':  { '$gte': min_lat },
+			'latitude':  { '$lte': max_lat }
+		
+		}
+
+		page_size = int(request.args.get('count', PAGE_SIZE))
+		page = int(request.args.get('page', 1))
+		ip_data = db.ip_data.count(query)
 		return app.response_class(
 			response=dumps(ip_data),
 			status=200,
@@ -67,3 +128,4 @@ def after_request(response):
 		if headers:
 			response.headers['Access-Control-Allow-Headers'] = headers
 	return response
+
